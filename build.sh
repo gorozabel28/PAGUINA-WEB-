90da39cb1863d8e42db37d97798f76c4d3f0e03a
gorozabel28 <153968411+gorozabel28@users.noreply.github.com>
2024-05-19 15:43:58 -0500

    Update build.sh script for deployment

--- build.sh
+++ build.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
-# exit on error
+# Exit on error
 set -o errexit
+
+# Install dependencies
 pip install -r requirements.txt
+
+# Collect static files
 python manage.py collectstatic --no-input
+
+# Make migrations
 python manage.py makemigrations --no-input
+
+# Apply migrations
 python manage.py migrate
 
-if [[ $CREATE_SUPERUSER ]];
-then
-    python manage.py createsuperuser --no-input
-    fi
+# Create superuser if the environment variable is set
+if [[ $CREATE_SUPERUSER ]]; then
+    python manage.py createsuperuser --no-input || true
+fi
